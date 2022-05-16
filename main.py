33517fced8223cf1dc6288ea3f82d059a3284d8f
ambition_echo <ambition_echo@outlook.com>
2022-05-16 13:00:45 +0800

    test

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from telegram.ext import Updater
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 import sys
 
-print (len(sys.argv))
-print (str(sys.argv))
+token=sys.argv[1]
+chatid=sys.argv[2]
+message=sys.argv[3:-2]
+button_name=sys.argv[-2]
+button_url=sys.argv[-1]
+
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+dispatcher.bot.send_message(chat_id=chatid, text=message, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(button_name, url=button_url)]]))
