cf1063e5ab1fa6709cc49a52517245dd2a6949db
ambition_echo <ambition_echo@outlook.com>
2022-05-16 13:27:13 +0800

    test

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from telegram.ext import Updater
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-import sys
+
 import os
 
-token=sys.argv[1]
-chatid=sys.argv[2]
-message=""
-button_name=sys.argv[-2]
-button_url=sys.argv[-1]
-
-for i in sys.argv[3:-2]:
-    message+=i+'\n\n'
-
-os.getenv('INPUT_MESSAGE')
-os.environ.get('INPUT_MESSAGE')
-message = os.environ['INPUT_MESSAGE']
+token=os.getenv('INPUT_TOKEN')
+chatid=os.getenv('INPUT_CHATID')
+message=os.getenv('INPUT_MESSAGE')
+button=os.getenv('INPUT_BUTTON')
+button_name=os.getenv('INPUT_BUTTON_NAME')
+button_url=os.getenv('INPUT_BUTTON_URL')
 
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
-
-dispatcher.bot.send_message(chat_id=chatid, text=str(message), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(button_name, url=button_url)]]))
+if(button):
+    dispatcher.bot.send_message(chat_id=chatid, text=str(message), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(button_name, url=button_url)]]))
+else:
+    dispatcher.bot.send_message(chat_id=chatid, text=str(message))
